bce5a44c0662010fa6e9982679813106d22397c4
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2010-03-11 17:18:36 -0800

    Delete accidental copy of moves_from_evid

--- pokemon.py
+++ pokemon.py
@@ -42,46 +42,6 @@ def moves_from_evid(evid):
     stages = {None: 0}
     for (id, name, evparent), group in itertools.groupby(cur, lambda x: (x[:3])):
         parents[id] = evparent
-        movesets.append(((id, name), list(x[3:] for x in group)))
-
-    def stage(id):
-        if id is None:
-            return 0
-        try:
-            evparent = parents[id]
-        except IndexError:
-            raise ValueError('no parent')
-        if evparent in stages:
-            return 1 + stages[evparent]
-        return 1 + stage(evparent)
-
-    for id in parents.keys():
-        stages[id] = stage(id)
-
-    # order the pokemon
-    movesets.sort(key=lambda x: stage(x[0][0]))
-
-    return movesets
-
-def moves_from_evid(evid):
-    _connect()
-    query = """
-        SELECT p.id, p.name, p.evolution_parent_pokemon_id,
-               pm.level, m.name
-        FROM pokemon p
-        JOIN pokemon_moves pm ON pm.pokemon_id = p.id
-        JOIN moves m ON pm.move_id = m.id
-        WHERE pm.version_group_id = ?
-            AND p.evolution_chain_id = ?
-            AND pm.pokemon_move_method_id = 1
-        ORDER BY p.id, pm.level, pm."order"
-    """
-    cur = _conn.execute(query, [LATEST_VERSION, evid])
-    movesets = []
-    parents = {}
-    stages = {None: 0}
-    for (id, name, evparent), group in itertools.groupby(cur, lambda x: (x[:3])):
-        parents[id] = evparent
         movesets.append(((id, name), [x[3:] for x in group]))
 
     def stage(id):
